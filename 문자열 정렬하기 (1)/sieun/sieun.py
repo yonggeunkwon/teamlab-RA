39e305fba4f36075b25f38faeb363f3670ec4d6b
sieun <sieun2580@naver.com>
2024-01-05 23:54:40 +0900

    [문자열 정렬하기 (1)] sieun - 코드수정

--- 문자열 정렬하기 (1)/sieun/sieun.py
+++ 문자열 정렬하기 (1)/sieun/sieun.py
@@ -1,14 +1,15 @@
 def solution(my_string):
     answer = []
 
-    for x in my_string:
-        if x.isdigit():  # 문자가 숫자인 경우에만
-            answer.append(int(x))  # 문자를 정수로 반환하여 리스트에 추가
+    for i in my_string:
+        if i.isdigit():  # 문자가 숫자인 경우에만
+            answer.append(int(i))  # 문자를 정수로 반환하여 리스트에 추가
 
     answer.sort()  # 리스트 오름차순 정렬
     return answer
 
 
-my_string = "hi12392"
-result = solution(my_string)
-print(result)
+if __name__ == "__main__":
+    my_string = "hi12392"
+    result = solution(my_string)
+    print(result)
