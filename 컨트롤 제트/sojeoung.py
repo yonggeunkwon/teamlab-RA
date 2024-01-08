73039a5bf0d962805d77d2a5d6857f1e7422cc4e
QHdld <sj041006@naver.com>
2024-01-08 19:45:08 +0900

    [컨트롤 제트]_sojeoung

--- 컨트롤 제트/sojeoung.py
+++ 컨트롤 제트/sojeoung.py
@@ -1,16 +1,30 @@
-s="1 2 Z 3"
-new=list(s.split())
-f_list=[]
+# s="1 2 Z 3"
+# new=list(s.split())
+# f_list=[]
 
-for i in range(len(new)):
-    if i+1>=len(new):
-        break
-    elif new[i+1] == 'Z':
-        new[i]=''
-        new[i+1]=''
+# for i in range(len(new)):
+#     if i+1>=len(new):
+#         break
+#     elif new[i+1] == 'Z':
+#         new[i]=''
+#         new[i+1]=''
 
-if new[i].isdigit() == True:
-    f_list.append(int(new[i])) 
+# if new[i].isdigit() == True:
+#     f_list.append(int(new[i])) 
 
-q=sum(f_list)
-print(q)
+# q=sum(f_list)
+# print(q)
+
+## stack & pop 사용해서 풀어보기
+
+s='1 2 Z 3'
+stack=[]
+
+for i in s:
+    if i.isdigit():
+        stack.append(int(i))
+    elif i == 'Z' :
+        f=stack.pop()
+
+result=sum(stack)
+print(result)
